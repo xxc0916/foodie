abd92b7a7876fc4f058fa3bc97ddca88154f6ebe
hill.he <hill.he@liulishuo.com>
2019-12-05 14:06:40 +0800

    update

--- doodie/assets/scripts/game.ts
+++ doodie/assets/scripts/game.ts
@@ -1,31 +1,44 @@
-// Learn TypeScript:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
-
-const {ccclass, property} = cc._decorator;
+import * as copy from "copy-to-clipboard";
+import axios from "axios";
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class NewClass extends cc.Component {
+  @property(cc.Label)
+  label: cc.Label = null;
 
-    @property(cc.Label)
-    label: cc.Label = null;
+  @property
+  text: string = "hello";
 
-    @property
-    text: string = 'hello';
+  // LIFE-CYCLE CALLBACKS:
 
-    // LIFE-CYCLE CALLBACKS:
+  // onLoad () {}
 
-    // onLoad () {}
+  start() {}
 
-    start () {
+  async touch() {
+    // copy("https://shard.llssite.com/?id=1&appid=2");
+    const data = await axios.get("http://10.180.3.137/votes/1/result");
+    cc.log(data);
+  }
 
+  touch2() {
+    const Request: any = this.GetRequest();
+    alert(Request.id);
+  }
+
+  GetRequest() {
+    var url = location.search; //获取url中"?"符后的字串
+    var theRequest = new Object();
+    if (url.indexOf("?") != -1) {
+      const str = url.substr(1);
+      const strs = str.split("&");
+      for (var i = 0; i < strs.length; i++) {
+        theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+      }
     }
+    return theRequest;
+  }
 
-    // update (dt) {}
+  // update (dt) {}
 }
